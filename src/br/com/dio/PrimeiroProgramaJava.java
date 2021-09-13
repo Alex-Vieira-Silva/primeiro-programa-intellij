5c746e98237067b529ac352fea4bfad8143a7d9c
Alex Vieira <Alexvieira.360@hotmail.com>
2021-09-13 11:03:58 -0300

    secudary commit

--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -5,11 +5,6 @@ import br.com.dio.model.Gato;
 public class PrimeiroProgramaJava {
     public static void main(String[] args) {
 
-       /* int a = 2;
-        int b = 6;
-
-        System.out.println("Hello World! " + (a+b));*/
-
         Gato gato = new Gato();
         gato.setNome("Sianeis");
         gato.setCor("Branca");
